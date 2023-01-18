a11e4cc22267c0f344dfdff7b42b89a180f29987
GlebNotss <gatsenko.g@mail.ru>
2023-01-18 17:01:33 +0300

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+path = r"/home/gleb/Desktop/projects/py_file_rename/img/"
+
+i = 1
+
+for file_name in os.listdir(path):
+    base_name, ext = os.path.splitext(file_name) 
+    base_name = base_name.strip().split(" ")
+    new_name = ("-").join(base_name).lower()
+
+    if ext.lower() not in ['.svg', '.jpeg', '.png']:
+        continue
+
+    abs_file_name = os.path.join(path, file_name)
+
+    new_abs_file_name = os.path.join(path, new_name + '.svg')
+    print(new_abs_file_name)
+    os.rename(abs_file_name, new_abs_file_name)
+    i += 1
